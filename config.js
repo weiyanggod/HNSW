e4e18136e9138782372a189016a279e4442488cb
weiyang <1460890475@qq.com>
2024-12-10 17:12:19 +0800

    提交

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 export default {
-	path: 'https://hnsw.xxqq.ffcode.cn/hn_extends',
-	// path: 'http://192.168.2.31:8043',
+	// path: 'https://hnsw.xxqq.ffcode.cn/hn_extends',
+	path: 'https://oa.hnswjt.com:16666/saftyManager/',
 	baseURL: 'https://hnsw.xxqq.ffcode.cn/hn_extends/api',
 	appId: 'wx6b3efabc1861d9cf'
 }
